91e18ede0f6eb2d73fa6428b64ed299e4c40ee1c
Azeem Khan <azeemkhanreal@gmail.com>
2022-04-15 18:29:40 +0530

    user regeration added

--- src/features/apiCalls.js
+++ src/features/apiCalls.js
@@ -11,3 +11,12 @@ export const login = async (dispatch, user) => {
     dispatch(loginFailure());
   }
 };
+export const register = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await publicRequest.post("/auth/register", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,24 +1,70 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import Header from "../components/Header";
+import { register } from "../features/apiCalls";
+
 const Register = () => {
+  const [user, setUser] = useState({});
+  const [confirmPwd, setConfirmPwd] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (confirmPwd !== user.password) {
+      setErrorMessage("Password not match");
+    } else {
+      register(dispatch, user);
+    }
+  };
   return (
     <Container>
       <Header />
       <FormContainer>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder="first name" />
-          <Input placeholder="last name" />
-          <Input placeholder="username" />
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Input placeholder="confirm password" />
+          <Input
+            placeholder="first name"
+            onChange={(e) => setUser({ ...user, firstname: e.target.value })}
+          />
+          <Input
+            placeholder="last name"
+            onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+          />
+          <Input
+            placeholder="username"
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+          <Input
+            placeholder="email"
+            type="email"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <Input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+          <Input
+            placeholder="confirm password"
+            type="password"
+            onChange={(e) => setConfirmPwd(e.target.value)}
+          />
           <Agreement>
             by creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
-          <Button>CREATE AN ACCOUNT</Button>
+          <Button onClick={handleClick} disabled={isFetching}>
+            CREATE AN ACCOUNT
+          </Button>
         </Form>
+        {error && (
+          <Error>
+            {errorMessage ? errorMessage : `Something went wrong ...`}
+          </Error>
+        )}
       </FormContainer>
     </Container>
   );
@@ -64,5 +110,13 @@ const Button = styled.button`
   background-color: teal;
   color: white;
   cursor: pointer;
+  &:disabled {
+    cursor: not-allowed;
+    color: green;
+  }
+`;
+const Error = styled.div`
+  color: red;
+  margin-top: 10px;
 `;
 export default Register;
